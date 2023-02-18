e99a8f5e697ee23dc7cb7c02345c0c5f0d5f18f6
hilmyveradin <hilmyveradin@gmail.com>
2023-02-18 20:37:37 +0700

    Initial Commit

---
+++ SudyStopwatch/SudyStopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudyStopwatchApp.swift
+//  SudyStopwatch
+//
+//  Created by Hilmy Veradin on 18/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SudyStopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
